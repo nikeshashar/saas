240218a91f6a33d131133c96bac4e9316c3d1de2
nikesh <nikeshashar@gmail.com>
2014-09-22 13:38:07 +0100

    Validating account presence

---
+++ app/models/account.rb
@@ -0,0 +1,3 @@
+class Account < ActiveRecord::Base
+  validates :subdomain, presence: true
+end
---
+++ spec/models/account_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe Account do 
+
+  describe 'validations' do 
+    it { should validate_presence_of :subdomain}
+    it { should validate_uniqueness_of :subdomain}
+
+    it { should allow_value('nikesh').for(:subdomain)}
+    it { should allow_value('test').for(:subdomain)}
+    it { should allow_value('nikeshashar').for(:subdomain)}
+    
+    it { should_not allow_value('www').for(:subdomain)}
+    it { should_not allow_value('WWW').for(:subdomain)}
+    it { should_not allow_value('.test').for(:subdomain)}
+    it { should_not allow_value('test/').for(:subdomain)}
+
+    it 'should validate case sensitive uniqueness' do 
+      create(:account, subdomain: 'Test')
+      expect(build(:account, subdomain: 'test')).not_to be_valid
+    end
+  end
+
+  describe 'associations' do 
+    it 'should have owner'
+  end
+end
