21cecf87f4f5dad5d71bf575eafb2b7ba7d86a57
nikesh <nikeshashar@gmail.com>
2014-09-22 17:07:57 +0100

    Updated rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,20 +3,39 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'spec_helper'
 require 'rspec/rails'
 require 'database_cleaner'
-require 'capybara/rspec'
+require 'capybara/rails'
+require 'launchy'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include Capybara::DSL
   config.include FactoryGirl::Syntax::Methods
   #config.include Devise::TestHelpers, type: :controller
-  config.order = "random"
+  # config.order = "random"
+
+ config.include Warden::Test::Helpers
+  Warden.test_mode!
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = false
 
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
   end
 
   config.before(:each) do
@@ -25,5 +44,6 @@ RSpec.configure do |config|
 
   config.after(:each) do
     DatabaseCleaner.clean
-  end
+    Warden.test_reset!
+  end 
 end
