674a745ba5f148ad677a5b30c3467678f4f9c521
nikesh <nikeshashar@gmail.com>
2014-10-11 11:35:27 +0100

    Updated accounts test

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe Account do 
 
   describe 'validations' do 
+    it { should validate_presence_of :owner}
     it { should validate_presence_of :subdomain}
     it { should validate_uniqueness_of :subdomain}
 
@@ -22,6 +23,6 @@ describe Account do
   end
 
   describe 'associations' do 
-    # it 'should have owner'
+    it { should belong_to :owner }
   end
 end
